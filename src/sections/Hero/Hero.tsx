7f551e1cb845461f2a7ac2f5d80ccb9012c674b9
mistifikatorcat <92595353+mistifikatorcat@users.noreply.github.com>
2023-05-25 23:46:28 +0300

    add some styles

--- src/sections/Hero/Hero.tsx
+++ src/sections/Hero/Hero.tsx
@@ -9,11 +9,16 @@ function Hero() {
     <section className={styles.hero}>
       <Image src={image} alt={'the book'} />
       <aside>
-        <h1>כל הקסם</h1>
-        <br />
-        <h1>— בידה</h1>
+        <h1>
+          כל הקסם
+          <br />— בידה
+        </h1>
         <p>
-        קורס מתחיליקורס מתחילים מ0-עד דבש קורס מתחילים מ0-עד דבש  vvקורס מתחילים מ0-עד דבש 
+          קורס מתחיליקורס מתחילים מ0-עד דבש
+          <br />
+          קורס מתחילים מ0-עד דבש vvקורס
+          <br />
+          מתחילים מ0-עד דבש
         </p>
         <div className={styles.buttonsWrapper}>
           <GradientButton text={'רכישה'} />
