c8d8fc63f5aae3663e206a9287c6f8f2eecf1098
mistifikatorcat <92595353+mistifikatorcat@users.noreply.github.com>
2023-05-25 19:49:13 +0300

    small fixes

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,26 @@
 import styles from './Header.module.scss';
-import logo from '../../../public/logo.png';
+import logo from '../../../public/images/logo.png';
+import Image from 'next/image';
+import Link from 'next/link';
+// import logo from '@public/images/logo.png';
 
 function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.wrapper}>
         <div className={styles.logo}>
-          <img className={styles.image} src={logo.src} />
+        <Image
+           src={logo}
+           alt='logo'
+           width={48}
+           height={48}
+           />
           <span>BeeTrade</span>
         </div>
       </div>
       <div className={styles.menu}>
-        <a>ספר</a>
-        <a>ביקורות</a>
+      <Link href='/' className={styles.link} >ספר</Link>
+        <Link href='/' className={styles.link}>ביקורות</Link>
       </div>
       <nav></nav>
     </header>
