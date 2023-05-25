b0b6073008f872273eb18f90e6f514125cf893f7
Matvei <matvesch@protonmail.com>
2023-05-25 20:11:32 +0300

    header fixes

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,28 +1,23 @@
 import styles from './Header.module.scss';
-import logo from '../../../public/images/logo.png';
 import Image from 'next/image';
 import Link from 'next/link';
-// import logo from '@public/images/logo.png';
+import logo from '@images/logo.png';
 
 function Header() {
   return (
     <header className={styles.header}>
-      <div className={styles.wrapper}>
-        <div className={styles.logo}>
-        <Image
-           src={logo}
-           alt='logo'
-           width={48}
-           height={48}
-           />
-          <span>BeeTrade</span>
-        </div>
+      <div className={styles.logo}>
+        <Image src={logo} alt="logo" width={48} height={48} />
+        <span>BeeTrade</span>
       </div>
-      <div className={styles.menu}>
-      <Link href='/' className={styles.link} >ספר</Link>
-        <Link href='/' className={styles.link}>ביקורות</Link>
-      </div>
-      <nav></nav>
+      <nav className={styles.menu}>
+        <Link href="/" className={styles.link}>
+          ספר
+        </Link>
+        <Link href="/" className={styles.link}>
+          ביקורות
+        </Link>
+      </nav>
     </header>
   );
 }
