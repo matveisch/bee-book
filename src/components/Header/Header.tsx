0e2603261472cea6fce2cdb2f444fce40ecb60b9
mistifikatorcat <92595353+mistifikatorcat@users.noreply.github.com>
2023-05-25 18:26:48 +0300

    added header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,22 @@
+import styles from './Header.module.scss';
+import logo from '../../../public/logo.png';
+
 function Header() {
-  return <div></div>;
+  return (
+    <header className={styles.header}>
+      <div className={styles.wrapper}>
+        <div className={styles.logo}>
+          <img className={styles.image} src={logo.src} />
+          <span>BeeTrade</span>
+        </div>
+      </div>
+      <div className={styles.menu}>
+        <a>ספר</a>
+        <a>ביקורות</a>
+      </div>
+      <nav></nav>
+    </header>
+  );
 }
 
 export default Header;
