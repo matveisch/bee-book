2ef3167cd442f5b5e770643fa2229db1ad9d1a16
mistifikatorcat <92595353+mistifikatorcat@users.noreply.github.com>
2023-05-26 21:11:36 +0300

    add description

--- src/sections/Description/Description.tsx
+++ src/sections/Description/Description.tsx
@@ -1,5 +1,16 @@
+import styles from './Description.module.scss';
+
+
+
 function Description() {
-  return <div></div>;
+  return (
+    <section className={styles.description}>
+      <p>
+      ספר דפוסי המסחר שלנו אינו רק אוסף של אסטרטגיות, הוא <span className={styles.gradient}> מדריך שלם </span>  למסחר מוצלח בשווקים הפיננסיים. בעזרתו תקבלו גישה לסודות של סוחרים מקצועיים ותלמדו
+<span className={styles.gradient}> איך להרוויח כסף בבורסה</span> כמשקיע מנוסה.
+      </p>
+    </section>
+  );
 }
 
 export default Description;
