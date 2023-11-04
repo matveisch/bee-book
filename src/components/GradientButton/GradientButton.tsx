2503780ffdd4bba3459ecf056d691252d812f737
Matvei <matvesch@protonmail.com>
2023-11-04 20:23:04 +0200

    Added onClick & type props to GradientButton, updated UI for BuyBook
    This commit introduces onClick and type props to the GradientButton component to increase its reusability by extending its functionality beyond static text display. Additionally, changes have been made to the BuyBook module's UI for a smoother user experience. The changes include the addition of input fields for entering a code and an error message section for feedback. These UI improvements enhance user interaction while attempting to buy a book.

--- src/components/GradientButton/GradientButton.tsx
+++ src/components/GradientButton/GradientButton.tsx
@@ -1,11 +1,16 @@
 import styles from './GradientButton.module.scss';
 import cart from '@images/cart-icon.svg';
 import Image from 'next/image';
+import { MouseEventHandler, ReactNode } from 'react';
 
-function GradientButton(props: { text: string }) {
-  const { text } = props;
+function GradientButton(props: {
+  text: string;
+  onclick?: MouseEventHandler<HTMLButtonElement>;
+  type?: 'submit' | 'button';
+}) {
+  const { text, onclick, type } = props;
   return (
-    <button className={styles.gradientButton}>
+    <button className={styles.gradientButton} onClick={onclick} type={type}>
       <Image src={cart} alt={'cart-icon'} />
       {text}
     </button>
--- src/sections/BuyBook/BuyBook.tsx
+++ src/sections/BuyBook/BuyBook.tsx
@@ -1,11 +1,30 @@
+'use client';
+
 import styles from './BuyBook.module.scss';
 import cover from '@images/book-cover.svg';
 import Image from 'next/image';
 import GradientButton from '@/components/GradientButton/GradientButton';
 import ShadowButton from '@/components/ShadowButton/ShadowButton';
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+
+const BOOK_CODE = '1234';
 
 function BuyBook(props: { dict: any }) {
   const { dict } = props;
+  const router = useRouter();
+  const [input, setInput] = useState('');
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+
+  function handleBook() {
+    if (input === BOOK_CODE) {
+      router.push(
+        'https://dkgzelvdxjuqppdhwkag.supabase.co/storage/v1/object/sign/bee-bucket/beetrade-book.pdf?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJiZWUtYnVja2V0L2JlZXRyYWRlLWJvb2sucGRmIiwiaWF0IjoxNjk5MTIxNzgwLCJleHAiOjE3MzA2NTc3ODB9.1TwClu32pBE9A6ptQOnYk4NgoLHJNMkG5aITCATjZuM&t=2023-11-04T18%3A16%3A19.985Z'
+      );
+    } else {
+      setErrorMessage('Wrong code');
+    }
+  }
 
   return (
     <div className={styles.buyBook} id="book">
@@ -16,7 +35,20 @@ function BuyBook(props: { dict: any }) {
         <h2>{dict.buyBook.invest}</h2>
         <p>{dict.buyBook.ruready}</p>
         <div className={styles.buttonsWrapper}>
-          <GradientButton text={dict.buyBook.buyNow} />
+          <div className={styles.inputContainer}>
+            <input
+              placeholder="Enter your code"
+              className={styles.codeInput}
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+            />
+            <p className={styles.errorMessage}>{errorMessage}</p>
+          </div>
+          <GradientButton
+            text={dict.buyBook.buyNow}
+            type="button"
+            onclick={handleBook}
+          />
           {/* <ShadowButton text={dict.buyBook.israeli} /> */}
         </div>
       </div>
